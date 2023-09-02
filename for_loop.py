e219258fd55ce4a109e5a8481a08aa5230d3bf0d
FarhanABD <farhanabdilah204@gmail.com>
2023-09-02 19:59:45 +0700

    Commit For loop & While loop

---
+++ for_loop.py
@@ -0,0 +1,14 @@
+# for i in range(10):
+#     print(i+1)
+#
+# for i in range(50,100 + 1,2):
+#     print(i)
+
+for i in "Farhan Abdillah":
+    print(i)
+
+import time
+for seconds in range(10,0,-1):
+    print(seconds)
+    time.sleep(1)
+print("Happy new year")
---
+++ while_loops.py
@@ -0,0 +1,9 @@
+name = ""
+age = ""
+
+while len(name) == 0:
+    name = input("Enter your name: ")
+while len(age) == 0:
+    age = input("Enter your fucking age: ")
+
+print("Hello " + name + " Your age is " + age)
